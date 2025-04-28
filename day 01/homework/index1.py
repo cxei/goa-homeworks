e8b1acf335daa80c9ec54b1b06fbe372f10d04a3
cxei <chkheidzeandria2011@gmail.com>
2025-04-28 22:50:38 +0400

    commit

---
+++ day 01/homework/index1.py
@@ -0,0 +1,14 @@
+
+# 3) შექმენით ცვლადები სადაც შეინახავთ ინფორმაციას თქვენს თავზე მაგ(name , age , address , height , weight) და ტერმინალში გამოიტანეთ სრული ინფორმაცია
+
+name="Andria"
+
+age=13
+
+address = "kedias 10"
+
+height = 1.83
+
+weight =58
+
+print(name,age,address,height,weight)
