f154ef9d246f48f83b50675795d6bf63a8af0fac
cxei <chkheidzeandria2011@gmail.com>
2025-06-20 21:50:35 +0400

    second commit

--- day 06/homework/task 03/homework3.py
+++ day 06/homework/task 03/homework3.py
@@ -1,6 +1,7 @@
-num1 = ("enter a random number")
-num2 = ("enter a second random number")
-num3 = ("enter a third random number")
-num4 = ("enter a fourth random number")
-num5 = ("enter a fifth random number")
-average = ((num1 + num2 +num3 +num4 +num5) / (5))
+num1 = int(input("enter a random number"))
+num2 = int(input("enter a second random number"))
+num3 = int(input("enter a third random number"))
+num4 = int(input("enter a fourth random number"))
+num5 = int(input("enter a fifth random number"))
+average = (num1 + num2 +num3 +num4 +num5) / 5
+print (average)
