8c465db0a634b327dd9f52fd3058342b38c993ee
cxei <chkheidzeandria2011@gmail.com>
2025-06-21 23:51:51 +0400

    commit

---
+++ day 08/homework/homework.py
@@ -0,0 +1,15 @@
+#integer ყველა მთელი რიცხვი , -♾️ -დან +♾️ მდე
+#string - ბრჭყალებში მოქცეული სიმბოლოები
+#float - ყველა ათწილადი
+
+string = "andria"
+integer = 50
+float = 0.7
+Integer = 33
+Float = 22.9 
+
+print(type(string))
+print(type(integer))
+print(type(float))
+print(type(Integer))
+print(type(Float))
